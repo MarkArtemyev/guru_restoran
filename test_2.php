aa4c21eb326d29f16a214f573aa573b5905ba3aa
Mark <61049418+MarkArtemyev@users.noreply.github.com>
2024-07-01 21:43:09 +0400

    Update test_2.php

--- test_2.php
+++ test_2.php
@@ -1,29 +1,21 @@
 <?php
 function highlightWords($text, $array_of_words) {
-    $foundWords = [];
-    
-    foreach ($array_of_words as $word) {
-        // Используем границы слова и флаги 'i' и 'u' для корректной работы с UTF-8
-        $pattern = '/\b' . preg_quote($word, '/') . '\b/iu';
-        $text = preg_replace_callback($pattern, function ($matches) use (&$foundWords) {
-            // Приводим слово к нижнему регистру для сравнения
-            $lowerWord = mb_strtolower($matches[0], 'UTF-8');
-            if (isset($foundWords[$lowerWord])) {
-                // Если слово уже найдено, возвращаем его без изменений
-                return $matches[0];
-            } else {
-                // Если слово найдено впервые, выделяем его и сохраняем
-                $foundWords[$lowerWord] = true;
-                return '[' . $matches[0] . ']';
-            }
-        }, $text, 1);
-    }
-    
-    return $text;
+    $pattern = '/\b(' . implode('|', array_map('preg_quote', $array_of_words)) . ')\b/ui';
+    $replaced = [];
+    $result = preg_replace_callback($pattern, function ($matches) use (&$replaced) {
+        if (isset($replaced[mb_strtolower($matches[0])])) {
+            return $matches[0];
+        }
+        $replaced[mb_strtolower($matches[0])] = true;
+        return '[' . $matches[0] . ']';
+    }, $text);
+
+    return $result;
 }
 
-// Пример использования:
-$text = 'Мама мыла раму';
-$array_of_words = ['ама', 'раму'];
-echo highlightWords($text, $array_of_words);
+$text = "Мама мыла раму и Вася мыла раму";
+$array_of_words = ["ама", "раму", "Вася"];
+
+$result = highlightWords($text, $array_of_words);
+echo $result;
 ?>
